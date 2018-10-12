6caa505013d6a3802d855781f8ee0e6e9c3949ba
jemsay07 <11234073+jemsay07@users.noreply.github.com>
2018-10-12 10:44:54 +0800

    Comments updated

--- search.php
+++ search.php
@@ -10,7 +10,7 @@
 get_header();
 ?>
 
-	<section id="primary" class="content-area">
+	<section id="primary" class="content-area col-12 col-md-12 col-lg-9">
 		<main id="main" class="site-main">
 
 		<?php if ( have_posts() ) : ?>
@@ -23,24 +23,24 @@ get_header();
 					?>
 				</h1>
 			</header><!-- .page-header -->
+			<div class="ajaw-post-wrap ajaw-g2 clearfix">
+				<?php
+				/* Start the Loop */
+				while ( have_posts() ) :
+					the_post();
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+					/**
+					 * Run the loop for the search to output the results.
+					 * If you want to overload this in a child theme then include a file
+					 * called content-search.php and that will be used instead.
+					 */
+					get_template_part( 'template-parts/content', 'search' );
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
+				endwhile;
 
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
+				the_posts_navigation(); ?>
+			</div>
+		<?php else :
 
 			get_template_part( 'template-parts/content', 'none' );
 
